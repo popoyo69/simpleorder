20a47b9a17a866763e17eeb767dec2a6f2dad24c
ali_akbar <ali_akbar@smltech.co.id>
2022-06-16 15:24:53 +0700

    initial commit

---
+++ src/main/java/com/kipas/simpleorder/SimpleorderApplication.java
@@ -0,0 +1,13 @@
+package com.kipas.simpleorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleorderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleorderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kipas/simpleorder/SimpleorderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kipas.simpleorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleorderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
